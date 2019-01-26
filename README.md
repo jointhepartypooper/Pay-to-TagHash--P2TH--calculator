9f33f93a895c29b8c3241ec549a7841c6798adf1
jointhepartypooper <>
2019-01-26 15:31:31 +0100

    calc with bitcore
    .

---
+++ README.md
@@ -0,0 +1,8 @@
+ 
+### Lints and fixes files
+```
+Pay to Taghash calculator from a transaction id.
+More information how a peercoin address is calculated, please go to
+https://jointhepartypooper.github.io/GraphicalPeercoinAddress/public/
+```
+ 
---
+++ p2th/src/components/P2TH.vue
@@ -0,0 +1,98 @@
+<template>
+  <div class="hello">
+    <h3>{{ msg }}</h3>Transaction hash:
+    <input
+     v-bind:class="{ notx: !isSHA }"
+      v-model="inputhash"
+      size="68"
+      placeholder="1bdae84eff34e15399335fc2a48c70fa6b0b9caf972e38b0e3bda106d223f668"
+    >
+    <h1>{{p2thPeercoinAddress}}</h1>
+  </div>
+</template>
+
+<script>
+var bitcore = require("bitcore-lib");
+
+//
+// Set peercoin as default network
+//
+
+bitcore.Networks.add({
+  name: "peercoin",
+  alias: "ppcoin",
+  pubkeyhash: 0x37,
+  privatekey: 0xb7,
+  scripthash: 0x75,
+  xpubkey: 0x0488b21e,
+  xprivkey: 0x0488ade4
+});
+
+bitcore.Networks.add({
+  name: "peercoin-testnet",
+  alias: "ppcoin-test",
+  pubkeyhash: 0x6f,
+  privatekey: 0xef,
+  scripthash: 0xc4,
+  xpubkey: 0x043587cf,
+  xprivkey: 0x04358394
+});
+
+bitcore.Networks.defaultNetwork = bitcore.Networks.get("peercoin");
+
+export default {
+  name: "P2TH",
+  props: {
+    msg: String
+  },
+  data() {
+    return {
+      inputhash: ""
+    };
+  },
+  computed: {
+    isSHA: function() {
+      return !!this.inputhash
+        ? this.inputhash.match(/^[a-f0-9]{64}$/i) !== null
+        : false;
+    },
+    p2thPeercoinAddress: function() {
+     
+      if (this.isSHA) {
+        var rawinput = this.inputhash;
+   
+        var value = Buffer.from(rawinput);
+        var hash = bitcore.crypto.Hash.sha256(value);
+        var bn = bitcore.crypto.BN.fromBuffer(hash);
+
+        var address = new bitcore.PrivateKey(bn).toAddress();
+    
+        return address.toString();  
+      } else {
+        return "";
+      }
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="less">
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+.notx{
+  border: 2px solid salmon;
+}
+</style>
